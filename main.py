f19179c2d699591ca27e4315bdc2b90fc48fe7fd
zlemihl <terence.a.oneill@gmail.com>
2018-03-27 22:01:04 -0500

    fixed 404 error

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 app.config['DEBUG']= True
-app.route("/")
-heady="""
-<!DOCTYPE html>
-<html>
-	<head>
-		<style>
-			form {
-				background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-			}
-			textarea {
-				margin: 10px 0;
-                width: 540px;
-                height: 120px;
-			}
-		</style>
-	</head>
-"""
+
+@app.route('/')
+#heady = """<!DOCTYPE html>
+#<html>
+#	<head>
+#		<style>
+#			form {
+#				background-color: #eee;
+#                padding: 20px;
+#                margin: 0 auto;
+#                width: 540px;
+#                font: 16px sans-serif;
+#                border-radius: 10px;
+#			}
+#			textarea {
+#				margin: 10px 0;
+#                width: 540px;
+#                height: 120px;
+#			}
+#		</style>
+#	</head>"""
 def index():
-	wc_form= heady+"""
+	wc_form="""
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<style>
+				form {
+					background-color: #eee;
+	                padding: 20px;
+	                margin: 0 auto;
+	                width: 540px;
+	                font: 16px sans-serif;
+	                border-radius: 10px;
+				}
+				textarea {
+					margin: 10px 0;
+	                width: 540px;
+	                height: 120px;
+				}
+			</style>
+		</head>
 		<body>
 			<form method="POST">
 				<label for="rot">Pick a Number</label>
